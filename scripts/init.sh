d1cf7a81820fc14a2dc585230bb0ad4fc28c0452
chhsiao <hsiao.chuanheng@gmail.com>
2013-03-16 11:19:01 +0800

    simplifying init process

--- scripts/init.sh
+++ scripts/init.sh
@@ -18,38 +18,21 @@ sed 's/^  exports = {};/  var exports = {};/g' ${filename} > ${filename}.tmp
 mv ${filename}.tmp ${filename}
 
 #PYRAMID
-echo "[INFO] to init pyramid"
+echo "[INFO] pcreate"
 
-is_using_pyramid=`./scripts/check_using_pyramid.pl "${VIRTUAL_ENV}" ${angular_brunch_seed_livescript_current_dir}`
-
-if [ "${is_using_pyramid}" == "false" ]
-then
-  echo "[WARNING] not is using pyramid or VIRTUAL_ENV is not set properly"
-  exit 0
-fi
-
-#LIB_PYRAMID
-LIB_PYRAMID="${VIRTUAL_ENV}/lib/python3.3/site-packages/pyramid-1.4-py3.3.egg/pyramid"
-if [ ! -d "${LIB_PYRAMID}" ]
-then
-  LIB_PYRAMID="${VIRTUAL_ENV}/lib/python3.3/site-packages/pyramid"
-fi
-  
 #templates
-echo "to link templates/* ../templates"
-the_basename=`pwd|sed 's/.*\///g'`
-echo "the_basename: ${the_basename}"
-cd ..
-ln -s ${the_basename}/templates ./
-cd ${angular_brunch_seed_livescript_current_dir}
+echo "to link templates"
+cd __/app
+ln -s ../../templates ./
+cd ../..
 
 #setup.py
-echo "[INFO] to setup.py: current_dir:"
-pwd
-SETUP_PY="../setup.py"
-cp scripts/setup.py ${SETUP_PY}
+echo "[INFO] to setup.py"
+cp scripts/setup.py __/app
 
-cd ..
+cd __/app
 python setup.py develop
 python setup.py egg_info
+
+
 cd ${angular_brunch_seed_livescript_current_dir}
