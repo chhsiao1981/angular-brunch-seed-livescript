5f1532cf93a1ef9e14cdeaa06f20334d528237b5
chhsiao <hsiao.chuanheng@gmail.com>
2013-03-16 12:11:27 +0800

    good passing all the tests. embed virtualenv into __

--- scripts/init.sh
+++ scripts/init.sh
@@ -17,7 +17,7 @@ filename=node_modules/LiveScript-brunch/vendor/prelude-browser-0.6.0.js
 sed 's/^  exports = {};/  var exports = {};/g' ${filename} > ${filename}.tmp
 mv ${filename}.tmp ${filename}
 
-#PYRAMID
+#pcreate
 echo "[INFO] pcreate"
 
 #templates
